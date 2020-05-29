e7197b9cd7e9c28e8598ef15849d7671d71d9dcc
Andrey Polyakov <andrey@polyakov.im>
2020-05-29 18:21:24 +0300

    Немного обновляем код

--- js/script.js
+++ js/script.js
@@ -1,73 +1,69 @@
 'use strict';
 
-const score = document.querySelector('.score'),
-      tap = document.querySelector('.tap'),
-      buy = document.querySelectorAll('.newButtonForBuy'),
-      howCost = document.querySelector('.howCost');
-
 const options = {
-      money: 1,
-      boost: 0,
-      number: 0,
-      cost: 0,
-      cash: 1
-    }    
+    money: 1,
+    boost: 0,
+    number: 0,
+    cost: 0,
+    cash: 1
+};
 
 let someMoney = options.cost + options.boost;
 
-tap.addEventListener('click', () => {
-    if (options.boost == 0) {
-        buy[0].innerHTML = `<p> Boost x1 </p>`;
-    } else if (options.boost == 1) { // Заработало!
-        someMoney+=1;
-        buy[0].innerHTML = `<p>Требуется: ${options.cash} </p>`;
-    } else if (options.boost == 2) {
-        someMoney+=2;
-        buy[0].innerHTML = `<p>Требуется: ${options.cash} </p>`;
-    } else if (options.boost == 3) {
-        someMoney+=3;
-        buy[0].innerHTML = `<p>Требуется: ${options.cash} </p>`;
-    } else if (options.boost == 4) {
-        someMoney+=4;
-        buy[0].innerHTML = `<p>Требуется: ${options.cash} </p>`;
-    } else if (options.boost == 5) {
-        someMoney+=5;
-        buy[0].innerHTML = `<p>Требуется: ${options.cash} </p>`;
-    } else if (options.boost == 6) {
-        someMoney+=6;
-        buy[0].innerHTML = `<p>Требуется: ${options.cash} </p>`;
-    } else if (options.boost == 7) {
-        someMoney+=7;
-        buy[0].innerHTML = `<p>Требуется: ${options.cash} </p>`;
-    } else if (options.boost == 8) {
-        someMoney+=8;
-        buy[0].innerHTML = `<p>Требуется: ${options.cash} </p>`;
-    } else if (options.boost == 9) {
-        someMoney+=9;
-        buy[0].innerHTML = `<p>Требуется: ${options.cash} </p>`;
-    } else if (options.boost == 10) {
-        someMoney+=10;
-        buy[0].innerHTML = `<p> Max </p>`;
-    }
-    score.innerHTML = `<p> Ваш счёт: ${++someMoney} монет </p>`;
-});
+const score = document.querySelector('.score'),
+    tap = document.querySelector('.tap'),
+    buy = document.querySelectorAll('.newButtonForBuy');
 
-buy[0].addEventListener('click', () => {
-    if(someMoney >= options.cash && options.boost < 10) {
+const buyButton = buy.length ? buy[0] : null;
+
+const addMoney = () => {
+    const {boost} = options;
+    // Увеличиваем в том случае, если уровень не больше 10
+    if (boost <= 10) {
+        someMoney += boost !== 0 ? boost : 1;
+        updateMoneyNeededCount(boost);
+        updateScore();
+    }
+};
+
+const buyBoost = () => {
+    if (someMoney >= options.cash) {
         someMoney -= options.cash;
         options.cash *= 3;
         options.boost += 1;
         options.cost += 1;
-        score.innerHTML = `<p> Ваш счёт: ${someMoney} монет </p>`;
-        buy[0].innerHTML = `<p>Поздравляем с покупкой!</p>`;
-    } else if (options.boost <= 10 && options.boost >= 9) {
-        buy[0].innerHTML = `<p>Max</p>`; 
-    } else {
-        buy[0].innerHTML = `<p>Требуется: ${options.cash} </p>`; 
+        updateScore();
+        buyButton.innerHTML = `<p>Поздравляем с покупкой!</p>`;
+        // Показываем поздравление на 1 секунду, потом отображаем текущее состояние счетчика
+        setTimeout(() => {
+            updateMoneyNeededCount(options.boost);
+        }, 1000);
     }
-});
+};
 
-buy[0].innerHTML = `<p>Кол-во: 0 </p>`;
+/**
+ * Простая функция, которая обновляет отображение количества монет
+ */
+const updateScore = () => {
+    score.innerHTML = `<p>Ваш счёт: ${someMoney} монет</p>`;
+};
 
-console.log(someMoney);
+/**
+ * тоже самое, но для отображения кол-ва денег для буста
+ * @param level
+ */
+const updateMoneyNeededCount = (level = 0) => {
+    buyButton.innerHTML = level >= 10
+        ? `<p> У вас максимальный уровень буста </p>`
+        : `<p>Требуется: ${options.cash} </p>`;
+};
+
+/**
+ * навешиваем события лишь только в том случае, если кнопка была найдена
+ */
+if (buyButton && tap) {
+    tap.addEventListener('click', addMoney);
+    buyButton.addEventListener('click', buyBoost);
+    buyButton.innerHTML = `<p>Кол-во: 0 </p>`;
+}
 
